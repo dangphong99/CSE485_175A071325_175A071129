c8af233380fad370ef6a9af71b3f6e03a40d5892
Your Name <you@example.com>
2019-10-24 11:45:25 +0700

    new

--- Project/js/index.js
+++
@@ -1,9 +0,0 @@
-function clickMenu1() {
-  var x = document.getElementById("nav1");
-  if (x.className == "topnav") {
-      x.className += "responsive";
-  } 
-  else {
-      x.className = "topnav";
-  }
-}
